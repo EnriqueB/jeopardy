e4c7c9b1d81a19ecc20aadf607e138e2fc32c377
Enrique Bonilla <enriqueBonilla93@outlook.com>
2015-04-27 15:52:28 -0500

    Creacion de clases

---
+++ src/java/juego/Geografia.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juego;
+
+/**
+ *
+ * @author Enrique
+ */
+public class Geografia {
+    String pista;
+    int valor;
+
+    public Geografia(String pista, int valor) {
+        this.pista = pista;
+        this.valor = valor;
+    }
+
+    public Geografia() {
+    }
+
+    public String getPista() {
+        return pista;
+    }
+
+    public void setPista(String pista) {
+        this.pista = pista;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+    
+}
