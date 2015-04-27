6e37d156da4a4da8b163788fb43ac46ca638904f
Enrique Bonilla <enriqueBonilla93@outlook.com>
2015-04-27 16:02:44 -0500

    Creacion de la clase jugador

---
+++ src/java/juego/Jugador.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juego;
+
+/**
+ *
+ * @author Enrique
+ */
+public class Jugador {
+    String nombre;
+    int ID, puntuacion;
+
+    public Jugador() {
+    }
+
+    public Jugador(String nombre, int ID, int puntuacion) {
+        this.nombre = nombre;
+        this.ID = ID;
+        this.puntuacion = puntuacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+    
+}
