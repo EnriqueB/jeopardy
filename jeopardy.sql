703be6588bf9f272531342d48a09def683f8b69e
Enrique Bonilla <enriqueBonilla93@outlook.com>
2015-04-27 16:30:51 -0500

    Query para crear la base de datos agregado

---
+++ jeopardy.sql
@@ -0,0 +1,92 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.4
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 23, 2014 at 08:10 PM
+-- Server version: 5.1.41
+-- PHP Version: 5.3.1
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `jeopardy`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `preguntas`
+--
+
+CREATE TABLE IF NOT EXISTS `preguntas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tema` varchar(30) NOT NULL,
+  `categoria` varchar(30) NOT NULL,
+  `pista` varchar(400) NOT NULL,
+  `respuesta` varchar(40) NOT NULL,
+  `puntuacion` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `preguntas`
+--
+
+INSERT INTO `preguntas` (`id`, `tema`, `categoria`, `pista`, `respuesta`, `puntuacion`) VALUES
+(1, 'Fisica', 'Magnetismo', 'Unidad del Sistem Internacional en la que se mide un campo magnetico', 'Tesla', 200),
+(2, 'Geografia', 'Montan;as', 'Montan;a mas alta del mundo', 'Everest', 100);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `usuario`, `password`) VALUES
+(1, 'juan', 'juan');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jugadores`
+--
+
+CREATE TABLE IF NOT EXISTS `jugadores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(30) NOT NULL,
+  `puntuacion` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `jugadores`
+--
+
+INSERT INTO `jugadores` (`id`, `nombre`, `puntuacion`) VALUES
+(1, 'Pedro', 200),
+(2, 'Felipe', 500),
+(3, 'Maria', 700),
+(4, 'Jaime', 0);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
