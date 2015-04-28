5477217855ee7d06bf39a00d1b7d0523467fa396
Enrique Bonilla <enriqueBonilla93@outlook.com>
2015-04-28 03:02:46 -0500

    ya el ultimo

--- src/java/juego/DBHandler.java
+++ src/java/juego/DBHandler.java
@@ -26,39 +26,6 @@ public class DBHandler {
         }
     }
 
-//    public static void storeMessage(Mensaje mensaje) {
-//        try {
-//            Statement statement = connection.createStatement();
-//            String de = mensaje.getDe();
-//            String para = mensaje.getPara();
-//            String contenido = mensaje.getContenido();
-//            String query = "insert into mensajes (de, para, contenido) values ('" + de + "','" + para + "','" + contenido + "')";
-//            statement.executeUpdate(query);
-//            statement.close();
-//        } catch (SQLException ex) {
-//            Logger.getLogger(DBhandler.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//
-//    }
-
-//    public static ArrayList getMessages(String para) {
-//        ArrayList list = new ArrayList();
-//        try {            
-//            Statement statement = connection.createStatement();
-//            ResultSet results = statement.executeQuery("SELECT de, contenido, contenido FROM mensajes where para='"+para+"'");
-//            while (results.next()) {
-//                String de=results.getString(1);
-//                String contenido=results.getString(2);
-//                Mensaje mensaje = new Mensaje(de, para, contenido);
-//                list.add(mensaje);
-//            }
-//            statement.close();
-//            
-//        } catch (SQLException ex) {
-//            Logger.getLogger(DBhandler.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//        return list;
-//    }
 
     public static int logIn(String user, String pass){
         try{
